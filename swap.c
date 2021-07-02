e950303259caab4433faa268fbbbdd0790d3fae2
Adithya9901 <85027490+Adithya9901@users.noreply.github.com>
2021-07-02 09:29:47 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -14,7 +14,13 @@ void swap_max(int arr[],int l,int pos)
       t=i;
      }
  }
+ if(max!=arr[pos])
+ {int k=arr[pos];
+  arr[pos]=arr[t];
+  arr[t]=k;
  }
+ 
+}
  
  void ssort(int arr[],int l)
  {
